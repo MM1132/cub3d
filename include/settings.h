cca91ebb90c1dee37a452b39ed656a56e438fffc
Robert Reimann <rreimann@2-H-6.42heilbronn.de>
2025-04-30 18:39:05 +0200

    Make player speed slower

--- include/settings.h
+++ include/settings.h
@@ -6,7 +6,7 @@
 /*   By: rreimann <rreimann@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/24 15:32:39 by rreimann          #+#    #+#             */
-/*   Updated: 2025/04/30 18:36:02 by rreimann         ###   ########.fr       */
+/*   Updated: 2025/04/30 18:38:28 by rreimann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@
 // Player
 // This does not define the speed itself directly
 // But it defines the increase amount of the palyer's speed
-# define PLAYER_ACCELERATION 1.5
+# define PLAYER_ACCELERATION 0.7
 // Defines the speed at which the speed will be decreasing over time
 # define PLAYER_SPEED_DECREASE_MULTIPLIER 0.74
 # define PLAYER_ROTATION_SPEED 0.005
