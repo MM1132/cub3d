c1f4e8d50dc4fa9d387c8dce9d7e4780b893d85c
Robert Reimann <rreimann@2-H-4.42heilbronn.de>
2025-05-06 17:34:51 +0200

    Added small.cub map for testing

--- src/render/overlay/help.c
+++ src/render/overlay/help.c
@@ -6,7 +6,7 @@
 /*   By: rreimann <rreimann@student.42heilbronn.de> +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 12:02:20 by rreimann          #+#    #+#             */
-/*   Updated: 2025/05/06 17:16:24 by rreimann         ###   ########.fr       */
+/*   Updated: 2025/05/06 17:34:29 by rreimann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,20 @@
 
 void	put_help()
 {
-	static char	*texts[7] = {
+	static char	*texts[8] = {
 		"Controls:",
 		"move with WASD and MOUSE",
 		"left_arrow -> look left",
 		"right_arrow -> look right",
 		"g -> toggle minimap grid",
 		"r -> toggle minimap rotation",
-		"c -> toggle minimap rays"
+		"c -> toggle minimap rays",
+		"t -> toggle minimap target"
 	};
 	int			index;
 
 	index = 0;
-	while (index < 7)
+	while (index < 8)
 	{
 		mlx_put_string(g_mlx, texts[index], 20, MAX_MINIMAP_SIZE + 2 * MINIMAP_MARGIN + index * 20);
 		index++;
